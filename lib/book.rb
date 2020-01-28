e406850ea5930ab69f8620a0568b974decdd8e7b
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2020-01-28 18:50:15 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,14 +1,38 @@
 class Book
-  def initialize(booktitle)
-    @title = booktitle
+  def initialize(title)
+    @title = title
   end
   def title
     @title
   end
-  def author=(bookauthor="Agatha Christie")
-    @author = bookauthor
+  def author=(author)
+    @author = author
+  end
+  def author
+    @author
+  end
+  def page_count=(num)
+    @page_count = num
+  end
+  def page_count
+    @page_count
+  end
+  def genre=(genre)
+    @genre = genre
+  end
+  def genre
+    @genre
+  end
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
   end
 end
 book = Book.new("gets initialized with a title")
 book.title
+book.author = "Agatha Christie"
 book.author
+book.page_count = 272
+book.page_count
+book.genre = "Mystery"
+book.genre
+book.turn_page
