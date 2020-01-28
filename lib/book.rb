a91e86c0fb754531409ed53b1c86ff2161e53a18
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2020-01-28 18:31:36 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,20 @@
-
-
+class Book
+  def initialized=(booktitle)
+    @title = booktitle
+    @author = "Agatha Christie"
+    @pages = 272
+  end
+  def title
+    @title
+  end
+  def author
+    @author
+  end
+  def page_count
+    @pages
+  end
+end
+book = Book.new("And Then There Were None")
+book.title
+book.author
+book.page_count
