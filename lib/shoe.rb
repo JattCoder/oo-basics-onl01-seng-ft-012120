43e483331112094e5d3d46c7ff07b99b66a4e116
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2020-01-28 19:08:45 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,52 @@
-# Make your shoe class here!
+class Shoe
+  def initialize(brand="Nike")
+    @brand = brand
+  end
+  def brand
+    @brand
+  end
+  def color=(color)
+    @color = color
+  end
+  def color
+    @color
+  end
+  def size=(size)
+    @size = size
+  end
+  def size
+    @size
+  end
+  def material=(material)
+    @material = material
+  end
+  def material
+    @material
+  end
+  def condition=(shoecondition)
+    if shoecondition == "old"
+      @condition = "new"
+    else
+      @condition = shoecondition
+    end
+  end
+  def condition
+    @condition
+  end
+  def cobble
+    puts "Your shoe is as good as new!"
+  end
+end
+shoe = Shoe.new("Adidas")
+shoe.brand
+shoe.color = "red"
+shoe.color
+shoe.size = 9.5
+shoe.size
+shoe.material = "suede"
+shoe.material
+shoe.condition = "tattered"
+shoe.condition
+shoe.cobble
+shoe.condition = "old"
+shoe.condition
