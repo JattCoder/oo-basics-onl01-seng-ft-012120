d8c40e7b3c0f2a650c5fef467060bd503af48e28
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2020-01-28 19:03:35 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,7 +5,47 @@ class Shoe
   def brand
     @brand
   end
-
+  def color=(color)
+    @color = color
+  end
+  def color
+    @color
+  end
+  def size=(size)
+    @size = size
+  end
+  def size
+    @size
+  end
+  def material=(material)
+    @material
+  end
+  def material
+    @material
+  end
+  def condition=(condition)
+    @condition
+  end
+  def condition
+    if @condition == "old"
+      @condition = "new"
+    end
+    @condition
+  end
+  def cobble 
+    @cobble = "Your shoe is as good as new!"
+  end
 end
 shoe = Shoe.new("Adidas")
 shoe.brand
+shoe.color = "red"
+shoe.color
+shoe.size = 9.5
+shoe.size
+shoe.material = "suede"
+shoe.material
+shoe.condition = "tattered"
+shoe.condition
+shoe.cobble
+shoe.condition = "old"
+shoe.condition
